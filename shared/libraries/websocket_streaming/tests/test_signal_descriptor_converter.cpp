8826960d192f024acbe8a91af8cb44dbaec0c4aa
nikolai.shipilov <nikolai.shipilov@promwad.com>
2025-12-11 13:10:04 +0100

    Fix warning

--- shared/libraries/websocket_streaming/tests/test_signal_descriptor_converter.cpp
+++ shared/libraries/websocket_streaming/tests/test_signal_descriptor_converter.cpp
@@ -46,13 +46,6 @@ public:
         return 0;
     }
 
-    virtual std::string id() const override
-    {
-        static unsigned int id = 0;
-        return std::to_string(id);
-        ++id;
-    }
-
     std::vector<std::pair<int, nlohmann::json>> metaInformations;
 };
 
