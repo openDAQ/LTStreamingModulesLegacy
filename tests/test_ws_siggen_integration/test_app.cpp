4eac894b1c85407feae69861cd8cee102a09632d
nikolai.shipilov <nikolai.shipilov@opendaq.com>
2026-01-30 16:09:52 +0100

    Fix missing dependencies

--- tests/test_ws_siggen_integration/test_app.cpp
+++ tests/test_ws_siggen_integration/test_app.cpp
@@ -1,14 +1,8 @@
-#include <opendaq/module_manager_init.h>
-#include <opendaq/opendaq_init.h>
 #include <coreobjects/util.h>
 #include <testutils/daq_memcheck_listener.h>
 
 int main(int argc, char** args)
 {
-    daq::daqInitializeCoreObjectsTesting();
-    daqInitModuleManagerLibrary();
-    daqInitOpenDaqLibrary();
-
     testing::InitGoogleTest(&argc, args);
 
     testing::TestEventListeners& listeners = testing::UnitTest::GetInstance()->listeners();
