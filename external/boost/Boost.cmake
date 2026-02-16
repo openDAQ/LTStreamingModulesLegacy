9cd7bba0bbca441e46980f90fd7ff224117dd3c4
nikolai.shipilov <nikolai.shipilov@opendaq.com>
2026-02-16 22:02:43 +0100

    Fix boost uuid dep

--- external/boost/Boost.cmake
+++ external/boost/Boost.cmake
@@ -10,3 +10,9 @@ if (${REPO_OPTION_PREFIX}_ENABLE_SIGGEN_INTEGRATION_TESTS)
         program_options
     )
 endif()
+
+if (${REPO_OPTION_PREFIX}_ENABLE_CLIENT)
+    opendaq_add_required_boost_libs(
+        uuid
+    )
+endif()
