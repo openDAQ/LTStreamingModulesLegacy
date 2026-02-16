ea3a750336754b525b1ad1b2131134aabcad109b
nikolai.shipilov <nikolai.shipilov@opendaq.com>
2026-02-16 13:08:43 +0100

    Add missing boost components

--- external/boost/Boost.cmake
+++ external/boost/Boost.cmake
@@ -4,3 +4,15 @@ opendaq_add_required_boost_libs(
     asio
     beast
 )
+
+if (${REPO_OPTION_PREFIX}_ENABLE_CLIENT)
+    opendaq_add_required_boost_libs(
+        uuid
+    )
+endif()
+
+if (${REPO_OPTION_PREFIX}_ENABLE_SIGGEN_INTEGRATION_TESTS)
+    opendaq_add_required_boost_libs(
+        program_options
+    )
+endif()
