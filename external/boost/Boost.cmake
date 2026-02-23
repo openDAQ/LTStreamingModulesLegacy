2dbb28cabf8fb0d3b91756acae2844545abb9d93
nikolai.shipilov <nikolai.shipilov@opendaq.com>
2026-02-23 14:22:01 +0100

    Replace templated cmake option name by full name

--- external/boost/Boost.cmake
+++ external/boost/Boost.cmake
@@ -5,13 +5,13 @@ opendaq_append_required_boost_components(
     beast
 )
 
-if (${REPO_OPTION_PREFIX}_ENABLE_SIGGEN_INTEGRATION_TESTS)
+if (DAQMODULES_LT_STREAMING_ENABLE_SIGGEN_INTEGRATION_TESTS)
     opendaq_append_required_boost_components(
         program_options
     )
 endif()
 
-if (${REPO_OPTION_PREFIX}_ENABLE_CLIENT)
+if (DAQMODULES_LT_STREAMING_ENABLE_CLIENT)
     opendaq_append_required_boost_components(
         uuid
     )
